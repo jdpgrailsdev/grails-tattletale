bc102daea1bd3782a8d4554f22759821bba1525d
Jonathan Pearlin <jonathan.pearlin@dealer.com>
2014-03-07 15:06:21 -0500

    Modified to support Grails 2.0 and up.

--- TattletaleGrailsPlugin.groovy
+++ TattletaleGrailsPlugin.groovy
@@ -4,7 +4,7 @@ class TattletaleGrailsPlugin {
 	// the Maven group ID
 	def groupId = 'org.grails.plugins'
     // the version or versions of Grails the plugin is designed for
-    def grailsVersion = "2.3 > *"
+    def grailsVersion = "2.0 > *"
     // resources that are excluded from plugin packaging
     def pluginExcludes = [
         "grails-app/views/error.gsp"
@@ -19,35 +19,4 @@ Grails plugin that adds support for generating JBoss Tattletale reports.  For mo
     def license = "APACHE"
 	def issueManagement = [ system: "github", url: "http://github.com/jdpgrailsdev/grails-tattletale/issues" ]
 	def scm = [ url: "http://github.com/jdpgrailsdev/grails-tattletale" ]
-
-    def doWithWebDescriptor = { xml ->
-        // TODO Implement additions to web.xml (optional), this event occurs before
-    }
-
-    def doWithSpring = {
-        // TODO Implement runtime spring config (optional)
-    }
-
-    def doWithDynamicMethods = { ctx ->
-        // TODO Implement registering dynamic methods to classes (optional)
-    }
-
-    def doWithApplicationContext = { ctx ->
-        // TODO Implement post initialization spring config (optional)
-    }
-
-    def onChange = { event ->
-        // TODO Implement code that is executed when any artefact that this plugin is
-        // watching is modified and reloaded. The event contains: event.source,
-        // event.application, event.manager, event.ctx, and event.plugin.
-    }
-
-    def onConfigChange = { event ->
-        // TODO Implement code that is executed when the project configuration changes.
-        // The event is the same as for 'onChange'.
-    }
-
-    def onShutdown = { event ->
-        // TODO Implement code that is executed when the application shuts down (optional)
-    }
 }
